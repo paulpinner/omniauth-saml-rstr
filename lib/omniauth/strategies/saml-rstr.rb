2898ef1589379f8b77e00e0ed8502ec3a1568b92
Josh Skeen <josh@joshskeen.com>
2012-07-28 10:37:44 -0400

    update namespaces, classnames, and directory names to reflect new strategy name

---
+++ lib/omniauth/strategies/saml-rstr.rb
@@ -0,0 +1,48 @@
+require 'omniauth'
+
+module OmniAuth
+  module Strategies
+    class SAML_RSTR
+      include OmniAuth::Strategy
+
+      autoload :AuthRequest,      'omniauth/strategies/saml-rstr/auth_request'
+      autoload :AuthResponse,     'omniauth/strategies/saml-rstr/auth_response'
+      autoload :ValidationError,  'omniauth/strategies/saml-rstr/validation_error'
+      autoload :XMLSecurity,      'omniauth/strategies/saml-rstr/xml_security'
+
+      option :name_identifier_format, "urn:oasis:names:tc:SAML:1.1:nameid-format:emailAddress"
+
+      def request_phase
+        request = OmniAuth::Strategies::SAML::AuthRequest.new
+        redirect(request.create(options))
+      end
+
+      def callback_phase
+        begin
+          response = OmniAuth::Strategies::SAML::AuthResponse.new(request.params['wresult'])
+          response.settings = options
+          @name_id  = response.name_id
+          @attributes = response.attributes
+
+          return fail!(:invalid_ticket, 'Invalid SAML Ticket') if @name_id.nil? || @name_id.empty? || !response.valid?
+          super
+        rescue ArgumentError => e
+          fail!(:invalid_ticket, 'Invalid SAML-RSTR Response')
+        end
+      end
+
+      uid { @name_id }
+
+      info do
+        {
+          :name  => @name_id
+        }
+      end
+
+      extra { { :raw_info => @attributes } }
+
+    end
+  end
+end
+
+OmniAuth.config.add_camelization 'saml', 'SAML'
---
+++ spec/omniauth/strategies/saml-rstr/auth_response_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe OmniAuth::Strategies::SAML_RSTR::AuthResponse do
+  let(:xml) { :rstr_response }
+  subject { described_class.new(load_xml(xml)) }
+
+  describe :initialize do
+    context "when the response is nil" do
+      it "should raise an exception" do
+        expect { described_class.new(nil) }.to raise_error ArgumentError
+      end
+    end
+  end
+# 2012-07-25T21:16:34.271Z
+ describe :session_expires_at do
+    it "should return the SessionNotOnOrAfter as a Ruby date" do
+      subject.session_expires_at.to_i.should == Time.new(2012, 07, 25, 21, 16, 34, 0).to_i
+    end
+  end
+
+  describe :name_id do
+    it "should load the name id from the assertion" do
+      subject.name_id.should == 'highgroove@thinkthroughmath.com'
+    end
+  end
+
+  describe :valid? do
+    it_should_behave_like 'a validating method', true
+  end
+
+  describe :validate! do
+    it_should_behave_like 'a validating method', false
+  end
+
+
+
+end
